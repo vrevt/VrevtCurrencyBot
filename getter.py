f985ad952ce7ae99b7b905238f24f99bddaca17b
vrevt <revtivan@gmail.com>
2020-02-02 20:23:02 +0300

    hot fix

--- getter.py
+++ getter.py
@@ -4,4 +4,4 @@ def get_btc():
     url = 'https://yobit.net/api/2/btc_usd/ticker'
     response = requests.get(url).json()
     price = response['ticker']['last']
-    return str(price) + ' usd'
+    return str(price) + ' usd/1 btc'
